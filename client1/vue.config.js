a53a37811fe69a7c6ae9362461afa148abd4c04f
Udbhav <udbhavs03@gmail.com>
2020-05-11 17:09:12 +0530

    add build config

--- client1/vue.config.js
+++ client1/vue.config.js
@@ -1,3 +1,21 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require("path");
+const CompressionPlugin = require("compression-webpack-plugin");
+
 module.exports = {
-  lintOnSave: false
+  outputDir: path.resolve(__dirname, "../server/public"),
+  lintOnSave: false,
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000/"
+      }
+    }
+  },
+  configureWebpack: {
+    devtool: "source-map"
+  },
+  chainWebpack(config) {
+    config.plugin("CompressionPlugin").use(CompressionPlugin);
+  }
 };
