9d04d65fc8e95e61da0628d73b5447174b6e4525
Udbhav <udbhavs03@gmail.com>
2020-06-02 11:32:32 +0530

    add template

--- client1/src/App.vue
+++ client1/src/App.vue
@@ -1,13 +1,31 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <vue-progress-bar></vue-progress-bar>
+    <app-header />
+    <transition name="fade" mode="out-in" v-if="cssLoaded">
+      <router-view id="main" />
+    </transition>
+
+    <h1 v-else>Loading</h1>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "@vue/composition-api";
+import { defineComponent, onMounted } from "@vue/composition-api";
+import AppHeader from "@/components/AppHeader.vue";
+import "@/assets/styles/main.scss";
 
 export default defineComponent({
-  name: "App"
+  name: "App",
+  components: {
+    AppHeader
+  },
+
+  setup() {
+    onMounted(() => {
+      // class to add navbar spacing
+      document.body.classList.add("has-navbar-fixed-top");
+    });
+  }
 });
 </script>
