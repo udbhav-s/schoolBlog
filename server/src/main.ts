cbbe87cc6b1a86947071546cf0dcf674ce1c4669
Udbhav <udbhavs03@gmail.com>
2020-06-02 15:03:19 +0530

    add dotenv

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
