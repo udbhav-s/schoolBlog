75b409920d0625257d9429a717831da219fa1fd6
Udbhav <udbhavs03@gmail.com>
2020-06-02 11:34:12 +0530

    init

---
+++ client1/src/components/HeroSection.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="hero is-info is-medium is-bold is-mobile">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
