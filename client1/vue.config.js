2767c3eb13a12b303838b0c637fd98026b56782b
Udbhav <udbhavs03@gmail.com>
2020-06-03 08:39:56 +0530

    fix proxy with npm update

--- client1/vue.config.js
+++ client1/vue.config.js
@@ -8,7 +8,8 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000/"
+        target: "http://localhost:3000/",
+        changeOrigin: true
       }
     }
   },
