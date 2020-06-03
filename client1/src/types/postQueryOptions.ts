10274d2e4048d829eb676eec041e5791ce5925db
Udbhav <udbhavs03@gmail.com>
2020-06-03 13:13:10 +0530

    add published field

--- client1/src/types/postQueryOptions.ts
+++ client1/src/types/postQueryOptions.ts
@@ -2,6 +2,7 @@ import { QueryOptions } from "./queryOptions";
 
 export interface PostQueryOptions extends QueryOptions {
   verified?: boolean | string;
+  published?: boolean;
   userId?: number;
   categoryId?: number;
   search?: string;
