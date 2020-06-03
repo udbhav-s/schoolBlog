92f7c6ef12cceb407c388d45369d2d55d257c532
Udbhav <udbhavs03@gmail.com>
2020-06-03 22:31:54 +0530

    add postId field

--- server/src/file/dto/fileUpload.dto.ts
+++ server/src/file/dto/fileUpload.dto.ts
@@ -1,7 +1,12 @@
-import { IsString, IsEnum } from "class-validator";
+import { IsString, IsEnum, IsInt } from "class-validator";
+import { Transform } from "class-transformer";
 
 export class FileUploadDto {
   @IsString()
   @IsEnum(["image", "attachment", "thumbnail"])
   type: string;
+
+  @IsInt()
+  @Transform(id => parseInt(id))
+  postId: number;
 }
