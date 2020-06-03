8010986bf57d22fa8d5972da2ff6505e5cd8af6f
Udbhav <udbhavs03@gmail.com>
2020-06-03 12:24:43 +0530

    add level descriptions

---
+++ client1/src/config/levelDescriptions.ts
@@ -0,0 +1,31 @@
+export default [
+  // Reader
+  `
+  Can see verified posts and comments. <br>
+  Cannot submit posts and comments.
+  `,
+  // Member
+  `
+  Can see verified posts and comments. <br>
+  Can submit posts for verification. <br>
+  Can submit comments on verified posts.
+  `,
+  // Author
+  `
+  Can see verified posts and comments. <br>
+  Submitted posts are automatically verified. <br>
+  Can submit comments on verified posts.
+  `,
+  // Moderator
+  `
+  Can see all posts and comments. <br>
+  Submitted posts are automatically verified. <br>
+  Can delete any post or comment.
+  `,
+  // Admin
+  `
+  All the permissions of moderator. <br>
+  Can change the permission level of other user. <br>
+  (Such as setting new moderators or authors, or demoting them).
+  `
+];
