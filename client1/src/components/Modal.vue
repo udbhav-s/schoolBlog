cec29c14c7b778b3261627c20cb2846eb9dbbff8
Udbhav <udbhavs03@gmail.com>
2020-06-06 16:31:42 +0530

    add modal

---
+++ client1/src/components/Modal.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="modal">
+    <div class="modal-background"></div>
+    <div class="modal-content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  name: "Modal"
+});
+</script>
