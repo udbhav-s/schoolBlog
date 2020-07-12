971986efdf36f58675da52797dad5eb4bd7b9c49
Udbhav <udbhavs03@gmail.com>
2020-07-12 16:29:18 +0530

    add search icon

--- client1/src/config/fontAwesome.ts
+++ client1/src/config/fontAwesome.ts
@@ -1,8 +1,8 @@
 import Vue from "vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faPencilAlt, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faPencilAlt, faTrash, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(faPencilAlt, faTrash);
+library.add(faPencilAlt, faTrash, faSearch);
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
