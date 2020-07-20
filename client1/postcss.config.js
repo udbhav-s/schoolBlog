f074bde0eae302b3f0bf54b6a54e4722eda00137
Udbhav <udbhavs03@gmail.com>
2020-07-20 22:10:20 +0530

    whitelist quill & filepond

--- client1/postcss.config.js
+++ client1/postcss.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
-    "vue-cli-plugin-tailwind/purgecss": {},
+    "vue-cli-plugin-tailwind/purgecss": {
+      whitelistPatterns: [/^ql/, /^filepond/]
+    },
     autoprefixer: {}
   }
 };
