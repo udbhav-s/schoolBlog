ae849a730c5a0bab94962de236b65b8d783d9dd7
Udbhav <udbhavs03@gmail.com>
2020-07-22 19:47:40 +0530

    serialize/deserialize from db

--- server/src/auth/session.serializer.ts
+++ server/src/auth/session.serializer.ts
@@ -1,15 +1,46 @@
+// import { PassportSerializer } from '@nestjs/passport';
+// import { Injectable } from '@nestjs/common';
+
+// @Injectable()
+// export class SessionSerializer extends PassportSerializer {
+//   serializeUser(user: any, done: (err: Error, user: any) => void): any {
+//     done(null, user);
+//   }
+//   deserializeUser(
+//     payload: any,
+//     done: (err: Error, payload: string) => void,
+//   ): any {
+//     done(null, payload);
+//   }
+// }
+
 import { PassportSerializer } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class SessionSerializer extends PassportSerializer {
-  serializeUser(user: any, done: (err: Error, user: any) => void): any {
-    done(null, user);
+  constructor(
+    private userService: UserService
+  ) {
+    super();
   }
-  deserializeUser(
-    payload: any,
-    done: (err: Error, payload: string) => void,
-  ): any {
-    done(null, payload);
+
+  serializeUser(user: any, done: CallableFunction): any {
+    done(null, user.id);
+  }
+
+  async deserializeUser(
+    userId: string,
+    done: CallableFunction,
+  ) {
+    try {
+      const user = await this.userService.getById(parseInt(userId));
+      if (!user) done(new NotFoundException(), null);
+      else done(null, user);
+    }
+    catch (err) {
+      done(err);
+    }
   }
 }
