c9108862224a0013dc66213f57d41bd16f85880d
Udbhav <udbhavs03@gmail.com>
2020-07-22 07:02:11 +0530

    add pre

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     "vue-cli-plugin-tailwind/purgecss": {
+      whitelist: ["pre"],
       whitelistPatterns: [/^ql/, /^filepond/]
     },
     autoprefixer: {}
