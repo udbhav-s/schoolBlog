a5ac7937c995e7dbf763f1ccc42ec59707d35903
Udbhav <udbhavs03@gmail.com>
2020-07-22 08:22:08 +0530

    add spinner

--- client/src/components/post/PostList.vue
+++ client/src/components/post/PostList.vue
@@ -1,37 +1,41 @@
 <template>
   <div>
-    <post-search v-if="showOptions" @search="search" @close="cancelSearch" />
-    <div v-else-if="searchable" class="text-center">
-      <button
-        class="button border-none text-xl px-4"
-        @click="showOptions = true"
-      >
-        <font-awesome-icon icon="search" class="mr-1" />
-        Search
-      </button>
-    </div>
+    <div v-if="!loading">
+      <post-search v-if="showOptions" @search="search" @close="cancelSearch" />
+      <div v-else-if="searchable" class="text-center">
+        <button
+          class="button border-none text-xl px-4"
+          @click="showOptions = true"
+        >
+          <font-awesome-icon icon="search" class="mr-1" />
+          Search
+        </button>
+      </div>
 
-    <div class="hr"></div>
+      <div class="hr"></div>
 
-    <div v-for="post in posts" :key="post.id">
-      <post-card :post="post" />
-    </div>
+      <div v-for="post in posts" :key="post.id">
+        <post-card :post="post" />
+      </div>
 
-    <div v-if="posts.length == 0" class="text-center my-6">
-      <h2>No Posts</h2>
-    </div>
+      <div v-if="posts.length == 0" class="text-center my-6">
+        <h2>No Posts</h2>
+      </div>
 
-    <div class="my-6 text-center">
-      <button v-if="hasMorePosts" class="button" @click="loadPosts">
-        Load More
-      </button>
+      <div class="my-6 text-center">
+        <button v-if="hasMorePosts" class="button" @click="loadPosts">
+          Load More
+        </button>
+      </div>
     </div>
+    <spinner v-else />
   </div>
 </template>
 
 <script lang="ts">
 import PostCard from "@/components/post/PostCard.vue";
 import PostSearch from "@/components/post/PostSearch.vue";
+import Spinner from "@/components/Spinner.vue";
 import { postService } from "@/services";
 import { Post, PostQueryOptions } from "@/types";
 import { defineComponent, ref } from "@vue/composition-api";
@@ -51,13 +55,15 @@ export default defineComponent({
   },
   components: {
     PostCard,
-    PostSearch
+    PostSearch,
+    Spinner
   },
 
   setup(props) {
     const posts = ref<Post[]>([]);
     const hasMorePosts = ref<boolean>(true);
     const showOptions = ref<boolean>(false);
+    const loading = ref<boolean>(false);
 
     let searchOptions = {} as PostQueryOptions;
     const options: PostQueryOptions = {
@@ -84,8 +90,11 @@ export default defineComponent({
       if (props.userId) opts.userId = props.userId;
       if (props.drafts) opts.published = false;
 
+      loading.value = true;
       const result = await postService.getAll(opts);
+      loading.value = false;
       if ("error" in result) throw result.message;
+
 
       if (result.data.length > 0) {
         result.data.forEach(post => posts.value.push(post));
@@ -112,7 +121,8 @@ export default defineComponent({
       showOptions,
       loadPosts,
       search,
-      cancelSearch
+      cancelSearch,
+      loading
     };
   }
 });
