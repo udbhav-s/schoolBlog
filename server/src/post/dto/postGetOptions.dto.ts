9c6aba6d802a04d8f139aba3567844b1f3c826fa
Udbhav <udbhavs03@gmail.com>
2020-07-25 15:55:29 +0530

    add true check

--- server/src/post/dto/postGetOptions.dto.ts
+++ server/src/post/dto/postGetOptions.dto.ts
@@ -5,12 +5,12 @@ import { Transform } from 'class-transformer';
 export class PostGetOptionsDto extends GetOptionsDto {
   @IsOptional()
   @IsBoolean()
-  @Transform(val => val === 'true')
+  @Transform(val => val === 'true' || val === true)
   verified?: boolean;
 
   @IsOptional()
   @IsBoolean()
-  @Transform(val => val === 'true')
+  @Transform(val => val === 'true' || val === true)
   published?: boolean;
 
   @IsOptional()
