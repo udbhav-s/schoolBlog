762657f6589daa62cb490c96b88a89252c3df61f
Udbhav <udbhavs03@gmail.com>
2020-07-25 14:49:46 +0530

    add transform

--- server/src/reply/dto/replyGetOptions.dto.ts
+++ server/src/reply/dto/replyGetOptions.dto.ts
@@ -1,7 +1,10 @@
-import { IsInt } from 'class-validator';
+import { IsInt, IsOptional } from 'class-validator';
 import { GetOptionsDto } from '../../common/dto/getOptions.dto';
+import { Transform } from 'class-transformer';
 
 export class ReplyGetOptionsDto extends GetOptionsDto {
+  @IsOptional()
   @IsInt()
+  @Transform(id => parseInt(id))
   commentId?: number;
 }
