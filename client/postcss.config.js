90b63ddc11058c2db22f0610bcc0d05d1424eaaf
Udbhav <udbhavs03@gmail.com>
2020-08-02 16:02:32 +0530

    dont purge list styles

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     "vue-cli-plugin-tailwind/purgecss": {
-      whitelist: ["pre", "dark"],
+      whitelist: ["pre", "dark", "ol", "ul"],
       whitelistPatterns: [/^ql/, /filepond/],
       whitelistPatternsChildren: [/^ql/, /filepond/, /prose/]
     },
