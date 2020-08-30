995fc288640d332c98231767ecb21ecbe27cd5e7
Udbhav <udbhavs03@gmail.com>
2020-08-30 14:56:25 +0530

    add s3 region for v4 signature

--- server/src/file/s3.ts
+++ server/src/file/s3.ts
@@ -2,7 +2,9 @@ import * as aws from 'aws-sdk';
 
 export const s3 = new aws.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  // signatureVersion: 'v4',
+  region: process.env.S3_REGION
 });
 
 export const bucketName = process.env.S3_BUCKET_NAME;
